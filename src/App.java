cff3878e06fc437b5e174122b8076ff6f3e8867c
github naotake32 <take32.nao@gmail.com>
2022-07-14 23:23:56 -0700

    2nd commit

--- src/App.java
+++ src/App.java
@@ -1,47 +1,148 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
+import com.mysql.cj.jdbc.Driver;
+
 public class App {
-	public static void main(String[] args) throws Exception {
-		
-		int inputNum = 0;
-		String inputName;
-		String inputDept;
-		int inputSalary = 0;
-		 List<Student> houseList = new ArrayList<Student>();
-		
-		Scanner scanner = new Scanner(System.in);
-		
-        System.out.println("Select an option:/n"
-        + "=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*\n"
-        + "1) Print all students:\n"
-        + "2) Add a new student:\n"
-        );
-        inputNum = scanner.nextInt();
+    public static void main(String[] args) throws Exception {
         
-        System.out.println(inputNum);
-        System.out.println("============================================\n");
-        System.out.println("============================================\n");
-        
-        if(inputNum == 1) {
-        	System.out.println("=========================================\n");
-        	System.out.println("-----------------------------------------\n");
-        	System.out.println("=========================================\n");
-//        	Server.getName();
-//        	Server.getDept();
-//        	Server.getSalary();
-        }else if(inputNum == 2) {
-        	System.out.println("Please enter a name:\n");
-        	inputName = scanner.nextLine();
-        	System.out.printf("Please enter %s's department:", inputName);
-        	inputDept = scanner.nextLine();
-        	System.out.printf("Please enter %s's salary:", inputName);
-        	inputSalary = scanner.nextInt();
-        	
+        String url = "jdbc:mysql://127.0.0.1:3306/a0522";
+        String user = "root";
+        String password = "password";
+
+        Scanner input = new Scanner(System.in);
+        ResultSet resultSet;
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+
+            Statement statement = conn.createStatement();
+
+            while(true){
+                System.out.println("Select an option\n"
+                +"1) Print all employees\n"
+                +"2) Add an employee\n\n\n");
+
+                int option = input.nextInt();
+
+                switch (option) {
+                    case 1:
+
+                        System.out.println("==================");
+                        resultSet = statement.executeQuery("SELECT * FROM employee_tbl");
+                        while(resultSet.next()){
+                            String name = resultSet.getString("name");
+                            String dept = resultSet.getString("dept");
+                            int salary = resultSet.getInt("salary");
+                            System.out.printf("%s - %s (%d)%n", name, dept, salary);
+                            System.out.println(" . . . . . . . . . . . . ");
+                        }
+                        System.out.println("==================");
+                        
+                        continue;
+                    case 2:
+                        input.nextLine();
+
+                        System.out.print("Please enter a name: ");
+                        String inputName = input.nextLine(); 
+                        System.out.print("Please enter a department: ");
+                        String inputDept = input.nextLine(); 
+                        System.out.print("Please enter a salary: ");
+                        int inputSalary = input.nextInt(); 
+
+                        String q = "INSERT INTO employee_tbl (name, dept, salary) VALUES (?, ?, ?)";
+                        PreparedStatement stmt = conn.prepareStatement(q);
+                        stmt.setString(1, inputName);
+                        stmt.setString(2, inputDept);
+                        stmt.setInt(3, inputSalary);
+
+                        stmt.executeUpdate();
+
+                        System.out.println("\n\n\n");
+
+                        continue;
+                
+                    default:
+                        System.out.println("Invalid option. Please try again.");
+                        continue;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        	
-      
-	
-	}
+
+
+    }
+
+    public void intro(){
+        String url = "jdbc:mysql://127.0.0.1:3306/a0522";
+        String user = "root";
+        String password = "password";
+        int rowsAffected = 0;
+
+        try {
+            //eåstablish connection
+            Connection conn = DriverManager.getConnection(url, user, password);
+
+            //create a statement object
+            Statement statement = conn.createStatement();
+
+            // statement.executeQuery("SELECT * FROM employee_tbl");
+
+            
+
+            
+            //create new employee
+            // rowsAffected = statement.executeUpdate("INSERT INTO employee_tbl (name, dept, salary) VALUES ('Mark','Marketing',5000)");
+            // System.out.println("Executed an INSERT statement = Rows affected: "+ rowsAffected);
+
+            // String name = "Benny";
+            // String dept = "IT";
+            // int salary = 3000;
+
+            // String qry = "INSERT INTO employee_tbl (name, dept, salary) VALUES (?, ?, ?)";
+            // PreparedStatement statement2 = conn.prepareStatement(qry);
+            // statement2.setString(1, name);
+            // statement2.setString(2, dept);
+            // statement2.setInt(3, salary);
+            // statement2.executeUpdate();
+
+
+            //ResultSet interface
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employee_tbl");
+            //Read the table
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("name") + " - " + resultSet.getString("dept") + " ("+resultSet.getString("salary")+")");
+            }
+
+
+            //Update a statement
+            // rowsAffected = statement.executeUpdate("UPDATE employee_tbl SET salary = 1 WHERE id = 1");
+            // System.out.println("Executed an UPDATE statment - Rows Affect: " + rowsAffected);
+
+            //Delete a single statement
+            rowsAffected = statement.executeUpdate("DELETE FROM employee_tbl WHERE id = 2");
+            System.out.println("Executed a DELETE statement - Rows Affect: "+ rowsAffected);
+
+
+
+
+            //ResultSet interface
+            resultSet = statement.executeQuery("SELECT * FROM employee_tbl");
+            //Read the table
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("name") + " - " + resultSet.getString("dept") + " ("+resultSet.getString("salary")+")");
+            }
+
+            
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
